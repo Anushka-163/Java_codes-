23960cb9d16125b0ff738cd2e403a2f1b019b3ae
Annu_163! <anushka7923singh@gmail.com>
2022-04-21 23:50:37 +0530

    Create problem_solvingQ3.java

---
+++ problem_solvingQ3.java
@@ -0,0 +1,12 @@
+# Java_codes-
+  class Solution {
+    public int[] runningSum(int[] nums) {
+        int n = nums.length;
+        int ans[]=new int[n];
+        int sum=0;
+        for(int i=0;i<n;i++){
+            sum+=nums[i];
+            ans[i]=sum;
+        }return ans;
+    }
+}
