6a920a1638d45e71151bf7fd561327a2bb2fface
Annu_163! <anushka7923singh@gmail.com>
2022-04-22 14:29:04 +0530

    Create problem_solvingQ4.java

---
+++ problem_solvingQ4.java
@@ -0,0 +1,13 @@
+# Java_codes-
+  class Solution {
+    public int maximumWealth(int[][] accounts) {
+        int n=accounts.length;
+        int max=0;
+        for(int i=0;i<n;i++){
+            int sum=0; 
+            for(int j=0;j<accounts[i].length;j++){
+                sum+=accounts[i][j];
+            }if(sum>=max) max=sum;
+        }return max;
+    }
+}
